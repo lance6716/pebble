4d1ff74027fdef7da199d7db40e5bf161bb59b17
lance6716 <lance6716@gmail.com>
2024-11-19 16:58:29 +0800

    switch to C zstd
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- sstable/compression_cgo_test.go
+++ sstable/compression_cgo_test.go
@@ -2,8 +2,8 @@
 // of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-//go:build nocase
-// +build nocase
+//go:build cgo
+// +build cgo
 
 package sstable
 
